f9ad0ce7ea968e76d3d37eeeb90e6d163158e762
Lam Nguyễn Mai <nguyenlamit86@gmail.com>
2016-08-14 15:43:36 +0700

    Environment.h v1.0
    Mr Lam

---
+++ MAS/core/environment.h
@@ -0,0 +1,29 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include "template.h"
+
+class Environment{
+public:
+    int x, y, grayScale;
+    byte *pixels;
+    bool createEnvironment(const char* fn, int dx, int dy, int grayScale);
+
+    inline int getIdx(int dx, int dy){
+        return x * y + dy * x + dx;
+    }
+
+    inline byte getValue(int dx, int dy){
+        return pixels[x * y + dy * x + dx];
+    }
+    inline void setValue(int dx, int dy, byte pix){
+        pixels[x * y + dy * x + dx] = t;
+    }
+
+
+    void getNeighbors(int idx, int *neigh, int &size);
+
+};
+
+
+#endif // ENVIRONMENT_H
---
+++ MAS/core/template.h
@@ -0,0 +1,13 @@
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+#include <istream>
+
+using namespace std;
+
+typedef unsigned int uint;
+typedef unsigned char byte;
+typedef unsigned short ushort;
+
+float gauss(int value, float mean, float varDis);
+
+#endif // TEMPLATE_H
